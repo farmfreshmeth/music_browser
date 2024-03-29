a5e5bc3786b9f9cbe5fa355e77e40e4fac842eb0
Farm Freshmeth <farmfreshmeth@gmail.com>
2024-03-29 09:03:29 -0400

    Remove debug

--- routes/item.js
+++ routes/item.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 router.get('/item/:item_id', async function(req, res, next) {
   var item = await req.app.locals.collection.item(req.params["item_id"]);
-  console.log(item);
   res.render('item', { item: item });
 });
 
