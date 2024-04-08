a16539523070f4ba4c2bfbd8772f647b9c9174e7
Farm Freshmeth <farmfreshmeth@gmail.com>
2024-04-08 13:43:01 -0400

    Scroll to top on reload

--- public/javascripts/tracklist.js
+++ public/javascripts/tracklist.js
@@ -9,9 +9,10 @@ $('a.lyrics').click((event) => {
 
   if ($('#track-container-' + position).is(':hidden')) {
     old_item_detail = $('#item-detail').replaceWith($('#track-container-' + position));
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
     $('#track-container-' + position).show();
   } else {
-    location.reload();
+    location.reload(); // I don't love it. requires another query
+    window.scrollTo(0, 0);
   }
 });
