05a79aea1329bf87784d24cc8d29344787119f04
Farm Freshmeth <farmfreshmeth@gmail.com>
2024-04-08 13:38:42 -0400

    Display lyrics on click.  Also close

---
+++ public/javascripts/tracklist.js
@@ -0,0 +1,17 @@
+/*
+  tracklist.js view
+*/
+
+$('a.lyrics').click((event) => {
+  event.preventDefault();
+  let position = $(event.target).data('position');
+  let old_item_detail = null;
+
+  if ($('#track-container-' + position).is(':hidden')) {
+    old_item_detail = $('#item-detail').replaceWith($('#track-container-' + position));
+    window.scrollTo(0,0);
+    $('#track-container-' + position).show();
+  } else {
+    location.reload();
+  }
+});
